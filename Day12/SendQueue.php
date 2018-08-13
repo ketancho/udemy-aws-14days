5a19610b7fb0bf1e9b0598bbc061cbf86724b38e
ketancho <kei.knzw@gmail.com>
2018-08-14 05:39:15 +0900

    Add simple SQS samples.

---
+++ Day12/SendQueue.php
@@ -0,0 +1,29 @@
+<?php
+require '../vendor/autoload.php';
+
+use Aws\Sqs\SqsClient;
+use Aws\Exception\AwsException;
+
+$client = new SqsClient([
+    'region' => 'ap-northeast-1',
+    'version' => '2012-11-05'
+]);
+
+$params = [
+    'MessageAttributes' => [
+        "Sender" => [
+            'DataType' => "String",
+            'StringValue' => "ketancho"
+        ]
+    ],
+    'MessageBody' => date('YmdHis'),
+    //TODO: Queue の URL を記載する
+    'QueueUrl' => '***'
+];
+
+try {
+    $result = $client->sendMessage($params);
+    var_dump($result);
+} catch (AwsException $e) {
+    error_log($e->getMessage());
+}
