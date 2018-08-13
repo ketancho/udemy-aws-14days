e0dea99b1af7de644450ce85d56d65744793032c
ketancho <kei.knzw@gmail.com>
2018-08-14 05:39:15 +0900

    Add simle SQS samples.

---
+++ Day12/ReceiveQueue.php
@@ -0,0 +1,35 @@
+<?php
+require '../vendor/autoload.php';
+
+use Aws\Sqs\SqsClient;
+use Aws\Exception\AwsException;
+
+//TODO: Queue の URL を記載する
+$queueUrl = "***";
+
+$client = new SqsClient([
+    'region' => 'ap-northeast-1',
+    'version' => '2012-11-05'
+]);
+
+try {
+    $result = $client->receiveMessage(array(
+        'AttributeNames' => ['SentTimestamp'],
+        'MaxNumberOfMessages' => 1,
+        'MessageAttributeNames' => ['All'],
+        'QueueUrl' => $queueUrl, // REQUIRED
+        'WaitTimeSeconds' => 0,
+    ));
+    if (count($result->get('Messages')) > 0) {
+        var_dump($result->get('Messages')[0]);
+        $result = $client->deleteMessage([
+            'QueueUrl' => $queueUrl, // REQUIRED
+            'ReceiptHandle' => $result->get('Messages')[0]['ReceiptHandle'] // REQUIRED
+        ]);
+    } else {
+        echo "No messages in queue. \n";
+    }
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
