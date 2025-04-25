8a444a97654a2a9b83d9f0ee38d9d6a6d75de80c
Kei Kanazawa <kei.knzw@gmail.com>
2025-04-25 11:50:09 +0900

    update for 2025

--- Day4/index.php
+++ Day4/index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
-    <title>Hello World</title>
+    <title>Simple Blog</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
   </head>
   <body>
