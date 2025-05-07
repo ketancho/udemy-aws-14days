25e0ba0ad31ab14725b3b4475831d3a0f7f163e4
Kei Kanazawa <kei.knzw@gmail.com>
2025-05-08 08:49:31 +0900

    2025 版に一部アップデート

--- Day12/send_queue.py
+++ Day12/send_queue.py
@@ -2,6 +2,6 @@ import boto3
 
 sqs_client = boto3.client('sqs')
 sqs_client.send_message(
-    QueueUrl='https://sqs.ap-northeast-1.amazonaws.com/761654548343/udemy-aws-14days-queue-2',
+    QueueUrl='[*作成した SQS URL に置き換える*]',
     MessageBody='Message from code.',
 )
