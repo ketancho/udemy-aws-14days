681a8afa222d32c1ad7ac535efbdbbaa8351a0d1
Kei Kanazawa <kei.knzw@gmail.com>
2025-05-08 09:56:07 +0900

    2025 版に一部アップデート

--- Day12/recieve_message.py
+++ Day12/recieve_message.py
@@ -1,11 +1,20 @@
 import boto3
 sqs_client = boto3.client('sqs')
+queue_url = 'https://sqs.ap-northeast-1.amazonaws.com/761654548343/udemy-aws-14days-queue-2'
 
 response = sqs_client.receive_message(
-    QueueUrl='https://sqs.ap-northeast-1.amazonaws.com/761654548343/udemy-aws-14days-queue-2',
+    QueueUrl=queue_url,
     WaitTimeSeconds=5
 )
-if 'Messages' in response:
-    message_id = response['MessageId']
-    message_body = response['Body']
+if 'Messages' not in response:
+    print('no message in queue.')
+else:
+    message = response['Messages'][0]
+    message_id = message['MessageId']
+    message_body = message['Body']
     print(message_id + ': ' + message_body)
+
+    sqs_client.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=message['ReceiptHandle']
+    )
