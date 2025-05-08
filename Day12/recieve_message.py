1a01289919b7fb174c75248b14aa870b2636a83e
Kei Kanazawa <kei.knzw@gmail.com>
2025-05-08 09:21:34 +0900

    2025 版に一部アップデート

--- Day12/recieve_message.py
+++ Day12/recieve_message.py
@@ -1,10 +1,8 @@
 import boto3
 
-sqs = boto3.resource('sqs')
-queue = sqs.Queue('https://sqs.ap-northeast-1.amazonaws.com/761654548343/udemy-aws-14days-queue-2')
-
-messages = queue.receive_messages(
-    MaxNumberOfMessages=10,
+sqs_client = boto3.client('sqs')
+response = sqs_client.receive_message(
+    QueueUrl='https://sqs.ap-northeast-1.amazonaws.com/761654548343/udemy-aws-14days-queue-2',
     WaitTimeSeconds=5
 )
-print(messages)
+print(response)
