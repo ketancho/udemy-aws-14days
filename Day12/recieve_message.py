e42b548b46b43c8f4f8b264bdbdeac2c2b7beac1
Kei Kanazawa <kei.knzw@gmail.com>
2025-05-08 09:00:09 +0900

    2025 版に一部アップデート

--- Day12/recieve_message.py
+++ Day12/recieve_message.py
@@ -2,5 +2,7 @@ import boto3
 
 sqs_client = boto3.client('sqs')
 sqs_client.receive_message(
-    QueueUrl='[*作成した SQS URL に置き換える*]'
+    QueueUrl='https://sqs.ap-northeast-1.amazonaws.com/761654548343/udemy-aws-14days-queue-2',
+    MaxNumberOfMessages=10,
+    WaitTimeSeconds=5
 )
